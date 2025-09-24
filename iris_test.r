fd856d648365b5adee74e04e1f784a6547ec7e1a
Jessica Klein <jessica.marie.klein@census.gov>
2025-09-24 14:06:51 -0400

    added a viz to this, too!

--- iris_test.r
+++ iris_test.r
@@ -14,3 +14,16 @@ group_by(Species) |>
 summarise(mean(Sepal.Width))
 
 # I would really like to add a visualization to this code file
+
+data(iris)
+cols <- c("setosa" = "tomato", 
+            "versicolor" = "steelblue", 
+            "virginica" = "darkgreen")
+
+plot(iris$Sepal.Length, iris$Sepal.Width,
+     pch = 19, cex = 1.1,
+     col = cols[iris$Species],
+     xlab = "Sepal.Length", ylab = "Sepal.Width",
+     main = "Iris: Sepal Length vs Width (base R)")
+
+legend("topright", legend = names(cols), pch = 19, col = cols)
